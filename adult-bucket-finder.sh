b0551a236e6137bff10987d8eebeceadbfe468af
Tyler Ramsbey <86263907+TeneBrae93@users.noreply.github.com>
2023-04-12 16:01:46 -0500

    Create adult-bucket-finder.sh

---
+++ adult-bucket-finder.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+print_help() {
+    echo "This is a tool that enumerates S3 buckets and returns the ones which are public. The public ones will be stored to target.txt"
+    echo "Usage: $0 [AWS_PROFILE_NAME]"
+}
+
+while getopts "h?:" opt; do
+    case "$opt" in
+    h|\?)
+        print_help
+        exit 0
+        ;;
+    esac
+done
+
+
+echo "-------------------------------"
+echo "Adult Bucket Finder is a tool that enumerates S3 Buckets in an AWS account to see if any are public." 
+echo "If they are public, it will be echoed to the screen in green writing as well as saved to 'targets.txt' for manual enumeration." 
+echo "Enjoy! -Tyler Ramsbey"
+echo "-------------------------------"
+
+for BUCKET in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do
+  if aws s3api get-bucket-acl --bucket $BUCKET | grep -q "URI=\"http\|URI=\"https";
+    then echo -e "\033[32mBucket $BUCKET is public\033[0m";
+    echo "$BUCKET" >> targets.txt;
+  else
+    echo -e "\033[31mBucket $BUCKET is not public\033[0m";
+  fi;
+done
