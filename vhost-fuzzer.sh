f9f93671bc2e373513c167d5090bce7cb66830bb
Tyler Ramsbey <86263907+TeneBrae93@users.noreply.github.com>
2023-12-14 09:44:37 -0600

    Create vhost-fuzzer.sh

---
+++ vhost-fuzzer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to display script usage
+show_help() {
+    echo "Usage: $0 <DOMAIN> <WORDLIST> <URL>"
+    echo "   -h, --help     Display this help and exit"
+    echo
+    echo "This script is a vhost fuzzer using ffuf for people who may forget the syntax."
+}
+
+# Check if the help option is provided
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    show_help
+    exit 0
+fi
+
+# Check if the required arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Error: Missing or incorrect arguments."
+    show_help
+    exit 1
+fi
+
+# Assign arguments to variables
+DOMAIN="$1"
+WORDLIST="$2"
+URL="$3"
+
+# Run ffuf command
+ffuf -H "Host: FUZZ.$DOMAIN" -H "User-Agent: PENTEST" -c -w "$WORDLIST" -u "$URL"
