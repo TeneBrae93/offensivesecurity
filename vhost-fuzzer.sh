5bf9904820e92bd34615e8013ea6dd0a9bdf91ec
Tyler Ramsbey <86263907+TeneBrae93@users.noreply.github.com>
2024-01-02 08:55:32 -0600

    Update vhost-fuzzer.sh
    - Added "fs" option for filtering based on size

--- vhost-fuzzer.sh
+++ vhost-fuzzer.sh
@@ -2,7 +2,7 @@
 
 # Function to display script usage
 show_help() {
-    echo "Usage: $0 <DOMAIN> <WORDLIST> <URL>"
+    echo "Usage: $0 <DOMAIN> <WORDLIST> <URL> <FS Filter>"
     echo "   -h, --help     Display this help and exit"
     echo
     echo "This script is a vhost fuzzer using ffuf for people who may forget the syntax."
@@ -15,7 +15,7 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
 fi
 
 # Check if the required arguments are provided
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
     echo "Error: Missing or incorrect arguments."
     show_help
     exit 1
@@ -25,6 +25,7 @@ fi
 DOMAIN="$1"
 WORDLIST="$2"
 URL="$3"
+FS="$4"
 
 # Run ffuf command
-ffuf -H "Host: FUZZ.$DOMAIN" -H "User-Agent: PENTEST" -c -w "$WORDLIST" -u "$URL"
+ffuf -H "Host: FUZZ.$DOMAIN" -H "User-Agent: PENTEST" -c -w "$WORDLIST" -u "$URL" -fs "$FS"
