7b0b2ce1576130a6c9d6483741c9a237543be90f
TeneBrae93 <tyler.ramsbey@rhinosecuritylabs.com>
2024-04-09 08:40:16 -0500

    Added a simply bash script that will use nmap to discover web servers and eye-witness to get screenshots. Useful for external pentests

---
+++ web-scanner.sh
@@ -0,0 +1,18 @@
+  #!/bin/bash
+  # Bash script that will use nmap to find web servers and then eye-witness to get screenshots of all the web servers 
+
+  # Check if the input file is provided
+  if [ -z "$1" ]; then
+    echo "Please provide a .txt file as input."
+    exit 1
+  fi
+
+  # Read each line from the input file
+  while IFS= read -r line; do
+    # Use nmap to scan common web ports on each line (IP address or hostname)
+    nmap -p 80,443,8080 "$line" | grep -E '^(80|443|8080)/open' | awk '{print $2}' >> web-servers.txt
+  done < "$1"
+
+  # Run eyewitness on the web servers
+  eyewitness --web -f web-servers.txt
+  done < "$1"
