8b7b9839236df11ffd50d99b9e296176dc626aa1
TeneBrae93 <tyler.ramsbey@rhinosecuritylabs.com>
2024-04-09 08:44:07 -0500

    - Needed to update nmap syntax

--- web-scanner.sh
+++ web-scanner.sh
@@ -10,7 +10,7 @@
   # Read each line from the input file
   while IFS= read -r line; do
     # Use nmap to scan common web ports on each line (IP address or hostname)
-    nmap -p 80,443,8080 "$line" | grep -E '^(80|443|8080)/open' | awk '{print $2}' >> web-servers.txt
+    nmap -p 80,443,8080 -iL "$1" | grep -E '^(80|443|8080)/open' | awk '{print $2}' >> web-servers.txt
   done < "$1"
 
   # Run eyewitness on the web servers
